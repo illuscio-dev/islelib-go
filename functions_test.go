c5260c1142ff44c44c2e9854deecaccfe0dc8936
Billy Peake <b.peake@illuscio.com>
2020-09-16 13:11:45 -0700

    updated with development from closed source template library

--- functions_test.go
+++ functions_test.go
@@ -1,12 +1,10 @@
-package tests
+package islelib
 
 import (
 	"github.com/stretchr/testify/assert"
-	"islelib"
 	"testing"
 )
 
-
 func TestCastSpell(test *testing.T) {
-	assert.Equal(test, "Avada Kedavra", islelib.CastSpell())
+	assert.Equal(test, "Avada Kedavra", CastSpell())
 }
